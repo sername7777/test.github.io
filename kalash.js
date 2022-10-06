9042ecd92a6d1a3633f907d1bf030c4f176c1366
sername7777 <109747000+sername7777@users.noreply.github.com>
2022-10-06 22:58:01 +0200

    Update kalash.js

--- kalash.js
+++ kalash.js
@@ -3,14 +3,14 @@ module.exports = {
     port: "3000", 
     prefix: "+", //prefix of bot
     client: "💀", 
-    client_id: "689334069628371068", // client id of bot
-    client_secret: "a9e1e1de7b425438d70ad49fbbfc6b4c85e06ae8f873e0f34659bcdc17c6d4dc", //client secret of bot
-    redirect_uri: "localhost", //redirect url de replit
+    client_id: "", // client id of bot
+    client_secret: "", //client secret of bot
+    redirect_uri: "", //redirect url de replit
     footer: "Discord ・ Nitro Event", //footer of cmd
   support: "", //support of bot
-    wehbook: "https://discord.com/api/webhooks/937476016350302228/HWOYgrotionE0pde1_FL8e5iu_PzIYjlvYSh-ZVxJNM8ptheLSqqzFU_nVMRQUnsQb9A", //wehbook de logs
-    owners: ["314812085013970954"], //owner of bot
-    authLink: `https://discord.com/api/oauth2/authorize?client_id=689334069628371068&redirect_uri=https%3A%2F%2FCoolerscrpit.realityzetsu.repl.co&response_type=code&scope=guilds.join%20identify`, //link of auth  bot
+    wehbook: "", //wehbook de logs
+    owners: [""], //owner of bot
+    authLink: ``, //link of auth  bot
 
 } //just change client id
 
